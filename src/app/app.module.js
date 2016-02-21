068b859da256344e98d16377519c12408593d5bc
Laurence Stokes <Laurence Stokes>
2016-02-21 01:35:08 +0000

    removed firebase from module includes in main module

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -6,11 +6,20 @@
       // Angular modules.
       'ngRoute',
 
-      // Third party modules.
-      'firebase',
-
       // Custom modules.
       'app.landing'
-    ]);
+    ])  
+    .config(configFunction);
+
+  configFunction.$inject = ['$routeProvider'];
+
+  function configFunction($routeProvider) {
+    $routeProvider.otherwise({
+      redirectTo: '/'
+    });
+  }
+
+  runFunction.$inject = ['$rootScope', '$location'];
+
 
 })();
